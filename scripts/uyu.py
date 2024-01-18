064de36760721e66af5c4537e1d5518e5e740994
Adrian Caricari <adrian.caricari9@gmail.com>
2024-01-18 17:15:13 -0500

    Resolved merge conflicts

--- scripts/uyu.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('Q:\\Users\\adria\\PycharmProjects\\Personal\\Projects\\ClimateAnalysisModel\\data\\CMIP5A_data\\CHIP5A_data.csv', newline='\n') as f:
-  reader = csv.reader(f)
-  for row in reader:
-    # do something here with `row`
-    break
